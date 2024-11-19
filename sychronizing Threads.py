b33f94986b747fbef0106072a2c67090e038ba49
ishng <157577426+ishng@users.noreply.github.com>
2024-11-19 13:24:39 +0800

    Update sychronizing Threads.py

--- sychronizing Threads.py
+++ sychronizing Threads.py
@@ -1,26 +1,27 @@
 import threading
 import time
 
-class myThread(Threading.Thread):
-    def __init__ (self, threadID, name, counter):
-        Threading.Thread.__init__(self)
+class myThread(threading.Thread):
+    def __init__ (self, threadID, name, Counter):
+        threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        self.counter = counter
+        self.Counter = Counter
     def run (self):
         print("starting" +self.name)
         threadLock.acquire()
-        print_time(self.name, self.counter, 3)
+        print_time(self.name, self.Counter, 3)
         threadLock.release()
-def print_time(ThreadName, delay, Counter)
-    while counter:
+def print_time(threadName, delay, Counter):
+    while Counter:
+        time.sleep(delay)
         print(f"{threadName}:{time.ctime(time.time())}")
-        counter -=1
+        Counter -=1
 
 threadLock = threading.Lock()
-threads[]
-Thread1 = myThread (1, "Thread -1", 1)
-Thread2 = myThread (2, "Thread -2", 2)
+threads=[]
+thread1 = myThread (1, "Thread -1", 1)
+thread2 = myThread (2, "Thread -2", 2)
 thread1.start()
 thread2.start()
 threads.append(thread1)
